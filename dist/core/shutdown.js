e0f692d7583ce80d17b9a611891de9c50d3315ec
Thanh Tri Nguyen <tri.nguyen@notabasement.com>
2021-07-03 13:24:31 +0700

    remove dist

--- dist/core/shutdown.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.shutdown = void 0;
-var shutdown = function (app) {
-    console.log("\nShutting down...");
-    app.close();
-};
-exports.shutdown = shutdown;
