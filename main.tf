54c81b3d1f9ac2d56723b16b5535b722715599e2
Thanh Tri Nguyen <tri.nguyen@notabasement.com>
2021-07-04 15:02:57 +0700

    test output

--- main.tf
+++ main.tf
@@ -6,9 +6,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "aws_region" {
-  default = "us-east-1"
+  # default = "us-east-1"
+  type = string
 }
 
 variable "ingress_rules" {
